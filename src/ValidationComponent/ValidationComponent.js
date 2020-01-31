908d99ca5585a67543b745f7ac20c63d8acb52c3
blaec <WVm8opYk5}>
2020-01-31 19:43:13 +0200

    3 - add validation text depending on text length

--- src/ValidationComponent/ValidationComponent.js
+++ src/ValidationComponent/ValidationComponent.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
 const validationComponent = (props) => {
-    return <p>The length of entered text is: {props.textLength} chars</p>;
+    const MIN_LENGTH = 5;
+    const MAX_LENGTH = 15;
+    let message;
+    if (props.textLength <= MIN_LENGTH) {
+        message = 'Text too short';
+    } else if (props.textLength >= MAX_LENGTH) {
+        message = 'Text long enough';
+    } else {
+        message='The length of entered text is: ' + props.textLength + ' chars';
+    }
+
+    return <p>{message}</p>;
 };
 
 export default validationComponent;
